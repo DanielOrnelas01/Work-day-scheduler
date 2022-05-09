b52ea025b0e4bee7011649ff01a4afe25fa168e2
dan <danielornelas01@yahoo.com>
2022-05-08 17:17:30 -0700

    should work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Work-day-scheduler
+
+Based on the given Acceptance Criteria from the customer I was able to create a functional calendar that will allow the user to save important events to a daily planner so that they can manage their time effectively.
+
+Features that were included based on the criteria given include being able to see the current day displayed at the top of the calendar, time blocks that run from 9AM to 5PM, with each time block dynamically showing whether each time block is in the past, present, or future. When the user clicks in to a time block they are able to enter a calendar event that they want to remember. By clicking the save button on the right of that time block they are able to save that event. If the user were to refresh, or close the page, upon reopening the application they will be presented with any events that they previously saved.
--- script.js
+++ script.js
@@ -1,56 +1,87 @@
+$(document).ready(function () {
 
-function displaydate() {
-    var currentDay = document.getElementById("currentDay")
-    setInterval(function () {
-        currentDay.textContent = new Date()
-    }, 1000)
-
-}
-
-function columns() {
-    for (var i = 0; i < 10; i++) {
-        var currentHour = new Date().getHours()
-        console.log(currentHour)
-        var row = document.createElement("div")
-        var one = document.createElement("div")
-        var two = document.createElement("input")
-        var three = document.createElement("div")
+    //add the current date to the #current date id in the HTML//
+    var dateString = moment().format("dddd, MMMM Do YYYY, h:mm a");
+    $("#currentDay").html(dateString);
     
-        row.setAttribute("class", "row")
-        one.setAttribute("class","col-2")
-        two.setAttribute("class","col-8")
-        two.setAttribute("type", "text")
-        three.setAttribute("class","col-2")
-
-        if(currentHour > i + 10){
-            
-        }
-
-        if (i + 10 < 13) {
-            one.textContent = i + 10 + "am"
-        } else {
-            one.textContent = i - 2 + "pm"
-        }
-
-        three.textContent = "💾"
-
-        row.appendChild(one)
-        row.appendChild(two)
-        row.appendChild(three)
-        document.querySelector(".container").appendChild(row)
+    //create html timeblocks with for loop starting at 9 am until 5pm//
+    var scheduledHours = [];
+    
+    for (var hour = 9; hour < 18; hour++) {
+        scheduledHours.push(moment({
+            hour
+        }).format('h  a'));
+        $('.container').append(`<div class="row time-block" data-time="${hour}"> 
+           <!--hour column-->
+               <div class="col-sm col-md-2 hour"> 
+                 <p class=dayHour>${moment({hour}).format('h  a')}</p>
+               </div> 
+               
+           <!--user input text area-->
+               <div class="col-sm col-md-8 d-flex description"> 
+                 <textarea class=textArea></textarea> 
+               </div> 
+          
+           <!--bootstrap saveBtn-->
+               <div class="col-sm col-md-2 saveBtn">
+               <i class="far fa-save fa-2x" id=icon></i>  
+               </div>`);
+      //  console.log({
+      //      hour
+      //  });
     }
-
-}
-
-
-
-
-
-
-
-
-function init() {
-    displaydate()
-    columns()
-}
-init()
+    
+    
+    //Check the hour of the current day to the hour represented in the HTML data-element to decide it's background color.
+    var m = moment();
+    $.each($(".time-block"), function (index, value) {
+        let dateHour = $(value).attr("data-time");
+        if (Number(dateHour) === m.hour()) {
+            $(this).find("textarea").addClass('present');
+        } else if (Number(dateHour) < m.hour()) {
+            $(this).find("textarea").addClass('past');
+        } else {
+            $(this).find("textarea").addClass('future');
+        }
+    });
+    
+    //Check for local storage to set value to the object
+    let timeObject = {};
+      if (localStorage.getItem('timeObject')) {
+          timeObject = JSON.parse(localStorage.getItem('timeObject'));
+      }else{
+        timeObject = {
+          '9': { time: "9", value: ""},
+          '10':{ time: "10", value: ""},
+          '11':{ time: "11", value: ""},
+          '12':{ time: "12", value: ""},
+          '13':{ time: "13", value: ""},
+          '14':{ time: "14", value: ""},
+          '15':{ time: "15", value: ""},
+          '16':{ time: "16", value: ""},
+          '17':{ time: "17", value: ""}
+        };
+      }
+    
+    //set value of timeObject to equal the user input for each row 
+    $(".time-block").each(function(){
+       $(this).find(".textArea").val(timeObject[$(this).attr("data-time")].value);
+       //console.log(this)
+      });
+    
+     //save value to local storage on click
+     $(".saveBtn").on('click', function(event){
+         //set timeObject time attribute
+         var timeValue = $(this).closest(".time-block").attr("data-time");
+        //set timeObject value attribute
+         var textValue = $(this).closest(".time-block").find(".textArea").val();
+         timeObject[timeValue].value = textValue;
+    
+      //save user input in each object to local storage
+         localStorage.setItem('timeObject', JSON.stringify(timeObject));
+    
+          //console.log(textValue)
+    
+     });
+     
+    });
